6b662d92f7a20fe331bd97c966fe08ed3f5c7ef7
Anthony Lukach <alukach@cadasta.org>
2017-07-28 14:42:34 -0600

    Refactor auth to lean on DRF's TokenAuthentication

--- rest_framework_tmp_scoped_token/auth.py
+++ rest_framework_tmp_scoped_token/auth.py
@@ -1,10 +1,10 @@
-from django.utils.six import text_type
-from rest_framework import authentication, exceptions, HTTP_HEADER_ENCODING
+from django.utils.translation import ugettext_lazy as _
+from rest_framework import authentication, exceptions
 
 from .token import TokenManager
 
 
-class TokenAuth(authentication.BaseAuthentication):
+class TokenAuth(authentication.TokenAuthentication):
     """
     Authentication scheme to authenticate with a token located in the
     'Authorization' header or as a 'TOKEN' query parameter.
@@ -15,26 +15,27 @@ class TokenAuth(authentication.BaseAuthentication):
     get_param = 'TOKEN'
 
     def authenticate(self, request):
-        auth = authentication.get_authorization_header(request)
-        if auth:
-            try:
-                split = self.keyword + ' '
-                if isinstance(split, text_type):
-                    # Follow DRF in making auth token byestring
-                    split = split.encode(HTTP_HEADER_ENCODING)
-                _, auth = auth.split()
-            except (IndexError, ValueError):
-                return
-        auth = auth or request.GET.get(self.get_param, '')
-        if auth:
-            try:
-                token = TokenManager.parse_token(auth)
-                if token.recipient:
-                    request.META['X-API-Token-Recipient'] = token.recipient
-                return token.authenticate(request)
-            except (AssertionError, ValueError) as e:
-                raise exceptions.AuthenticationFailed(e)
-            except TokenManager.SignatureExpired:
-                raise exceptions.AuthenticationFailed("Token has expired")
-            except TokenManager.BadSignature:
-                raise exceptions.AuthenticationFailed("Bad API token")
+        self.request = request
+
+        # Authenticate via header
+        output = super(TokenAuth, self).authenticate(request)
+        if output:
+            return output
+
+        # Authenticate via GET parameter
+        token = request.GET.get(self.get_param)
+        if token:
+            return self.authenticate_credentials(token)
+
+    def authenticate_credentials(self, key):
+        try:
+            token = TokenManager.parse_token(key)
+            if token.recipient:
+                self.request.META['X-API-Token-Recipient'] = token.recipient
+            return token.validate_request(self.request)
+        except (AssertionError, ValueError) as e:
+            raise exceptions.AuthenticationFailed(e)
+        except TokenManager.SignatureExpired:
+            raise exceptions.AuthenticationFailed(_("Token has expired"))
+        except TokenManager.BadSignature:
+            raise exceptions.AuthenticationFailed(_("Bad API token"))
